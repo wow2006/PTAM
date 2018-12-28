9c17a557e53c02fb3a5c621b1cffa6e4d586e29e
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-12-28 20:16:48 +0200

    Reformat MapViewer.hpp

--- include/PTAM/MapViewer.hpp
+++ include/PTAM/MapViewer.hpp
@@ -5,42 +5,87 @@
 //
 // Defines the MapViewer class
 //
-// This defines a simple map viewer widget, which can draw the 
+// This defines a simple map viewer widget, which can draw the
 // current map and the camera/keyframe poses within it.
 //
-#ifndef __MAP_VIEWER_H
-#define __MAP_VIEWER_H
+#pragma once
+
+#include <sstream>
+
+#include <TooN/TooN.h>
+#include <TooN/se3.h>
+
+#include "PTAM/GLWindow2.hpp"
 
 #include "Map.h"
-#include <TooN/TooN.h>
-using namespace TooN;
-#include <TooN/se3.h>
-#include <sstream>
-#include "GLWindow2.h"
 
+using namespace TooN;
+
+namespace PTAM {
 class Map;
 
-class MapViewer
-{
+class MapViewer {
 public:
+  /** 
+  * @brief 
+  * 
+  * @param map
+  * @param glw
+  */
   MapViewer(Map &map, GLWindow2 &glw);
+
+  /** 
+  * @brief 
+  * 
+  * @param se3CamFromWorld
+  */
   void DrawMap(SE3<> se3CamFromWorld);
+
+  /** 
+  * @brief 
+  * 
+  * @return 
+  */
   std::string GetMessageForUser();
-  
+
 protected:
   Map &mMap;
+
   GLWindow2 &mGLWindow;
-  
+
+  /** 
+  * @brief 
+  */
   void DrawGrid();
+
+  /** 
+  * @brief 
+  */
   void DrawMapDots();
-  void DrawCamera(SE3<> se3, bool bSmall=false);
+
+  /** 
+  * @brief 
+  * 
+  * @param se3
+  * @param bSmall
+  */
+  void DrawCamera(SE3<> se3, bool bSmall = false);
+
+  /** 
+  * @brief 
+  */
   void SetupFrustum();
+
+  /** 
+  * @brief 
+  * 
+  * @param se3WorldFromCurrent
+  */
   void SetupModelView(SE3<> se3WorldFromCurrent = SE3<>());
-  
+
   Vector<3> mv3MassCenter;
   SE3<> mse3ViewerFromWorld;
 
   std::ostringstream mMessageForUser;
 };
-
-#endif
+} // namespace PTAM
