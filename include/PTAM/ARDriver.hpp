bbf5f6395703b0fe7af55975abf217059c26e4db
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-12-28 19:12:31 +0200

    Reformat ARDriver.hpp

--- include/PTAM/ARDriver.hpp
+++ include/PTAM/ARDriver.hpp
@@ -8,41 +8,88 @@
 // distortion so that real and distorted virtual graphics can be
 // properly blended.
 //
-#ifndef __AR_Driver_H
-#define __AR_Driver_H
+#pragma once
 #include <TooN/se3.h>
-#include "ATANCamera.h"
-#include "GLWindow2.h"
-#include "OpenGL.h"
+
+#include <cvd/byte.h>
 #include <cvd/image.h>
 #include <cvd/rgb.h>
-#include <cvd/byte.h>
+
+#include "PTAM/ATANCamera.hpp"
+#include "PTAM/GLWindow2.hpp"
+#include "PTAM/OpenGL.hpp"
+
 #include "EyeGame.h"
 
 using namespace std;
 using namespace CVD;
 
-class ARDriver
-{
- public:
+namespace PTAM {
+
+class ARDriver {
+public:
+  /**
+   * @brief
+   *
+   * @param cam
+   * @param irFrameSize
+   * @param glw
+   */
   ARDriver(const ATANCamera &cam, ImageRef irFrameSize, GLWindow2 &glw);
-  void Render(Image<Rgb<byte> > &imFrame, SE3<> se3CamFromWorld);
+
+  /**
+   * @brief
+   *
+   * @param imFrame
+   * @param se3CamFromWorld
+   */
+  void Render(Image<Rgb<byte>> &imFrame, SE3<> se3CamFromWorld);
+
+  /**
+   * @brief
+   */
   void Reset();
+
+  /**
+   * @brief
+   */
   void Init();
- protected:
+
+protected:
   ATANCamera mCamera;
+
   GLWindow2 &mGLWindow;
+
+  /**
+   * @brief
+   */
   void DrawFadingGrid();
+
+  /**
+   * @brief
+   */
   void MakeFrameBuffer();
+
+  /**
+   * @brief
+   */
   void DrawFBBackGround();
+
+  /**
+   * @brief
+   */
   void DrawDistortedFB();
+
+  /**
+   * @brief
+   */
   void SetFrustum();
-  
+
   // Texture stuff:
   GLuint mnFrameBuffer;
   GLuint mnFrameBufferTex;
   GLuint mnFrameTex;
-  
+
   int mnCounter;
   ImageRef mirFBSize;
   ImageRef mirFrameSize;
@@ -52,4 +99,5 @@ class ARDriver
   // Eyeballs:
   EyeGame mGame;
 };
-#endif
+
+} // namespace PTAM
