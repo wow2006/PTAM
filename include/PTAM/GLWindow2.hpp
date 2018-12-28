04542f2caa3a982c5e37915f8a29442594a8352e
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-12-28 16:14:23 +0200

    Reformat GLWindow2.hpp

--- include/PTAM/GLWindow2.hpp
+++ include/PTAM/GLWindow2.hpp
@@ -8,68 +8,141 @@
 //  caption line for text display. Also provides some handy GL helpers
 //  and a wrapper for CVD's text display routines.
 
-#include <cvd/glwindow.h>
 #include <TooN/TooN.h>
+#include <cvd/glwindow.h>
 
 class GLWindowMenu;
 
+namespace PTAM {
 
-class GLWindow2 : public CVD::GLWindow, public CVD::GLWindow::EventHandler
-{
+class GLWindow2 : public CVD::GLWindow, public CVD::GLWindow::EventHandler {
 public:
+  /** 
+  * @brief 
+  * 
+  * @param irSize
+  * @param sTitle
+  */
   GLWindow2(CVD::ImageRef irSize, std::string sTitle);
-  
-  // The preferred event handler..
+
+  /** 
+  * @brief The preferred event handler..
+  */
   void HandlePendingEvents();
-  
-  // Menu interface:
+
+  /** 
+  * @brief Menu interface:
+  * 
+  * @param sName
+  * @param sTitle
+  */
   void AddMenu(std::string sName, std::string sTitle);
+
+  /** 
+  * @brief 
+  */
   void DrawMenus();
-  
-  // Some OpenGL helpers:
+
+  /** 
+  * @brief Some OpenGL helpers:
+  */
   void SetupViewport();
+
   void SetupVideoOrtho();
+
   void SetupUnitOrtho();
+
   void SetupWindowOrtho();
+
   void SetupVideoRasterPosAndZoom();
 
-  // Text display functions:
+  
+  /** 
+  * @brief Text display functions:
+  * 
+  * @param irPos
+  * @param s
+  */
   void PrintString(CVD::ImageRef irPos, std::string s);
+
   void DrawCaption(std::string s);
-  
-  // Map viewer mouse interface:
-  std::pair<TooN::Vector<6>, TooN::Vector<6> > GetMousePoseUpdate();
-  
+
+  /** 
+  * @brief Map viewer mouse interface:
+  * 
+  * @return 
+  */
+  std::pair<TooN::Vector<6>, TooN::Vector<6>> GetMousePoseUpdate();
 
 protected:
+  /** 
+  * @brief 
+  * 
+  * @param sCommand
+  * @param sParams
+  */
   void GUICommandHandler(std::string sCommand, std::string sParams);
-  static void GUICommandCallBack(void* ptr, std::string sCommand, std::string sParams);
-  
-  // User interface menus:
-  std::vector<GLWindowMenu*> mvpGLWindowMenus;
 
-  CVD::ImageRef mirVideoSize;   // The size of the source video material.
-  
+  /** 
+  * @brief 
+  * 
+  * @param ptr
+  * @param sCommand
+  * @param sParams
+  */
+  static void GUICommandCallBack(void *ptr, std::string sCommand,
+                                 std::string sParams);
 
-  // Event handling routines:
-  virtual void on_key_down(GLWindow&, int key);
-  virtual void on_mouse_move(GLWindow& win, CVD::ImageRef where, int state);
-  virtual void on_mouse_down(GLWindow& win, CVD::ImageRef where, int state, int button);
-  virtual void on_event(GLWindow& win, int event);
+  /** 
+  * @brief User interface menus:
+  */
+  std::vector<GLWindowMenu *> mvpGLWindowMenus;
+
+  CVD::ImageRef mirVideoSize; // The size of the source video material.
+
+  /** 
+  * @brief Event handling routines:
+  * 
+  * @param 
+  * @param key
+  */
+  virtual void on_key_down(GLWindow &, int key);
+
+  /** 
+  * @brief 
+  * 
+  * @param win
+  * @param where
+  * @param state
+  */
+  virtual void on_mouse_move(GLWindow &win, CVD::ImageRef where, int state);
+
+  /** 
+  * @brief 
+  * 
+  * @param win
+  * @param where
+  * @param state
+  * @param button
+  */
+  virtual void on_mouse_down(GLWindow &win, CVD::ImageRef where, int state,
+                             int button);
+
+  /** 
+  * @brief 
+  * 
+  * @param win
+  * @param event
+  */
+  virtual void on_event(GLWindow &win, int event);
+
   CVD::ImageRef mirLastMousePos;
 
   // Storage for map viewer updates:
   TooN::Vector<6> mvMCPoseUpdate;
   TooN::Vector<6> mvLeftPoseUpdate;
-  
-
 };
 
-
-
-
-
-
-
+} // namespace PTAM
 
 #endif
