1e19c56a11eee59ccf733b4e3fb420389ecc1020
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-12-29 18:27:49 +0200

    reformat HomographyInit.hpp

--- include/PTAM/HomographyInit.hpp
+++ include/PTAM/HomographyInit.hpp
@@ -1,66 +1,77 @@
-// -*- c++ -*- 
+// -*- c++ -*-
 // Copyright 2008 Isis Innovation Limited
 
-// HomographyInit.h 
-// Declares the HomographyInit class and a few helper functions. 
+// HomographyInit.h
+// Declares the HomographyInit class and a few helper functions.
 //
 // This class is used by MapMaker to bootstrap the map, and implements
 // the homography decomposition of Faugeras and Lustman's 1988 tech
 // report.
 //
 // Implementation according to Faugeras and Lustman
+#pragma once
 
-#ifndef __HOMOGRAPHY_INIT_H
-#define __HOMOGRAPHY_INIT_H
-#include <TooN/TooN.h>
-using namespace TooN;
-#include <TooN/se3.h>
 #include <vector>
 
-// Homography matches are 2D-2D matches in a stereo pair, unprojected
-// to the Z=1 plane.
-struct HomographyMatch
-{
+#include <TooN/TooN.h>
+#include <TooN/se3.h>
+
+using namespace TooN;
+
+namespace PTAM {
+/**
+ * @brief Homography matches are 2D-2D matches in a stereo pair, unprojected to
+ * the Z=1 plane.
+ */
+struct HomographyMatch {
   // To be filled in by MapMaker:
   Vector<2> v2CamPlaneFirst;
   Vector<2> v2CamPlaneSecond;
   Matrix<2> m2PixelProjectionJac;
 };
 
-// Storage for each homography decomposition
-struct HomographyDecomposition
-{
+/**
+ * @brief Storage for each homography decomposition
+ */
+struct HomographyDecomposition {
   Vector<3> v3Tp;
   Matrix<3> m3Rp;
   double d;
   Vector<3> v3n;
-  
+
   // The resolved composition..
   SE3<> se3SecondFromFirst;
   int nScore;
 };
 
-class HomographyInit
-{
+class HomographyInit {
 public:
-  bool Compute(std::vector<HomographyMatch> vMatches, double dMaxPixelError, SE3<> &se3SecondCameraPose);
+  /** 
+  * @brief 
+  * 
+  * @param vMatches
+  * @param dMaxPixelError
+  * @param se3SecondCameraPose
+  * 
+  * @return 
+  */
+  bool Compute(std::vector<HomographyMatch> vMatches, double dMaxPixelError,
+               SE3<> &se3SecondCameraPose);
+
 protected:
   Matrix<3> HomographyFromMatches(std::vector<HomographyMatch> vMatches);
   void BestHomographyFromMatches_MLESAC();
   void DecomposeHomography();
   void ChooseBestDecomposition();
   void RefineHomographyWithInliers();
-  
+
   bool IsHomographyInlier(Matrix<3> m3Homography, HomographyMatch match);
   double MLESACScore(Matrix<3> m3Homography, HomographyMatch match);
-  
+
   double mdMaxPixelErrorSquared;
   Matrix<3> mm3BestHomography;
   std::vector<HomographyMatch> mvMatches;
   std::vector<HomographyMatch> mvHomographyInliers;
   std::vector<HomographyDecomposition> mvDecompositions;
 };
-
-
-
-#endif
+} // namespace PTAM
