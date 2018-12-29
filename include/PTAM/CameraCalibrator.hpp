687a36d23791b3622f074542da2c36a60c63cb41
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-12-29 20:34:05 +0200

    Reformat CameraCalibrator.hpp

--- include/PTAM/CameraCalibrator.hpp
+++ include/PTAM/CameraCalibrator.hpp
@@ -1,34 +1,36 @@
 // -*- c++ -*-
 // Copyright 2008 Isis Innovation Limited
-
-#ifndef __CAMERACALIBRATOR_H
-#define __CAMERACALIBRATOR_H
-#include "CalibImage.h"
-#include "VideoSource.h"
-#include <gvars3/gvars3.h>
+#pragma once
 #include <vector>
-#include "GLWindow2.h"
 
-class CameraCalibrator
-{
+#include <gvars3/gvars3.h>
+
+#include "PTAM/CalibImage.hpp"
+#include "PTAM/GLWindow2.hpp"
+#include "PTAM/VideoSource.hpp"
+
+namespace PTAM {
+
+class CameraCalibrator {
 public:
   CameraCalibrator();
+
   void Run();
-  
+
 protected:
   void Reset();
   void HandleFrame(CVD::Image<CVD::byte> imFrame);
-  static void MainLoopCallback(void* pvUserData);
+  static void MainLoopCallback(void *pvUserData);
   void MainLoopStep();
   VideoSource mVideoSource;
-  
+
   GLWindow2 mGLWindow;
   ATANCamera mCamera;
   bool mbDone;
 
   std::vector<CalibImage> mvCalibImgs;
   void OptimizeOneStep();
-  
+
   bool mbGrabNextFrame;
   GVars3::gvar3<int> mgvnOptimizing;
   GVars3::gvar3<int> mgvnShowImage;
@@ -36,7 +38,8 @@ protected:
   double mdMeanPixelError;
 
   void GUICommandHandler(std::string sCommand, std::string sParams);
-  static void GUICommandCallBack(void* ptr, std::string sCommand, std::string sParams);
+  static void GUICommandCallBack(void *ptr, std::string sCommand,
+                                 std::string sParams);
 };
 
-#endif
+} // namespace PTAM
