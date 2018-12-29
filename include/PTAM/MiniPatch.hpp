ee1deba61d212e1acdcaa1f8916b36ba80d3d7bf
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-12-29 15:08:25 +0200

    reformat MiniPatch.hpp

--- include/PTAM/MiniPatch.hpp
+++ include/PTAM/MiniPatch.hpp
@@ -4,34 +4,61 @@
 // MiniPatch.h
 //
 // Declares MiniPatch class
-// 
+//
 // This is a simple pixel-patch class, used for tracking small patches
 // it's used by the tracker for building the initial map
+#pragma once
 
-#ifndef __MINI_PATCH_H
-#define __MINI_PATCH_H
-
-#include <cvd/image.h>
-#include <cvd/byte.h>
-#include <cvd/utility.h>
-#include <TooN/TooN.h>
-using namespace TooN;
 #include <vector>
 
-struct MiniPatch
-{
-  void SampleFromImage(CVD::ImageRef irPos, CVD::BasicImage<CVD::byte> &im);  // Copy pixels out of source image
-  bool FindPatch(CVD::ImageRef &irPos,           // Find patch in a new image
-		 CVD::BasicImage<CVD::byte> &im, 
-		 int nRange,
-		 std::vector<CVD::ImageRef> &vCorners,
-		 std::vector<int> *pvRowLUT = NULL);
-  
-  inline int SSDAtPoint(CVD::BasicImage<CVD::byte> &im, const CVD::ImageRef &ir); // Score function
-  static int mnHalfPatchSize;     // How big is the patch?
-  static int mnRange;             // How far to search? 
-  static int mnMaxSSD;            // Max SSD for matches?
-  CVD::Image<CVD::byte> mimOrigPatch;  // Original pixels
+#include <cvd/byte.h>
+#include <cvd/image.h>
+#include <cvd/utility.h>
+
+#include <TooN/TooN.h>
+
+using namespace TooN;
+
+namespace PTAM {
+
+struct MiniPatch {
+  /**
+   * @brief Copy pixels out of source image
+   *
+   * @param irPos
+   * @param im
+   */
+  void SampleFromImage(CVD::ImageRef irPos, CVD::BasicImage<CVD::byte> &im);
+
+  /**
+   * @brief Find patch in a new image
+   *
+   * @param irPos
+   * @param im
+   * @param nRange
+   * @param vCorners
+   * @param pvRowLUT
+   *
+   * @return
+   */
+  bool FindPatch(CVD::ImageRef &irPos, CVD::BasicImage<CVD::byte> &im,
+                 int nRange, std::vector<CVD::ImageRef> &vCorners,
+                 std::vector<int> *pvRowLUT = NULL);
+
+  /**
+   * @brief Score function
+   *
+   * @param im
+   * @param ir
+   *
+   * @return
+   */
+  int SSDAtPoint(CVD::BasicImage<CVD::byte> &im, const CVD::ImageRef &ir);
+
+  static int mnHalfPatchSize;         // How big is the patch?
+  static int mnRange;                 // How far to search?
+  static int mnMaxSSD;                // Max SSD for matches?
+  CVD::Image<CVD::byte> mimOrigPatch; // Original pixels
 };
 
-#endif
+} // namespace PTAM
