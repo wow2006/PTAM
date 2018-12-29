242957b12e533e26de4dae998f37532874eb1ced
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-12-29 20:16:08 +0200

    Reformat CalibCornerPatch.hpp

--- include/PTAM/CalibCornerPatch.hpp
+++ include/PTAM/CalibCornerPatch.hpp
@@ -1,18 +1,20 @@
 // -*- c++ -*-
 // Copyright 2008 Isis Innovation Limited
 
-#ifndef __CALIB_CORNER_PATCH_H
-#define __CALIB_CORNER_PATCH_H
-#include <TooN/TooN.h>
-using namespace TooN;
-#include <cvd/image.h>
-#include <cvd/byte.h>
+#pragma once
 
-class CalibCornerPatch
-{
+#include <TooN/TooN.h>
+
+#include <cvd/byte.h>
+#include <cvd/image.h>
+
+using namespace TooN;
+
+namespace PTAM {
+
+class CalibCornerPatch {
 public:
-  struct Params
-  {
+  struct Params {
     Params();
     Matrix<2> m2Warp();
     Vector<2> v2Pos;
@@ -20,27 +22,24 @@ public:
     double dMean;
     double dGain;
   };
-  
+
   CalibCornerPatch(int nSideSize = 8);
   bool IterateOnImage(Params &params, CVD::Image<CVD::byte> &im);
   bool IterateOnImageWithDrawing(Params &params, CVD::Image<CVD::byte> &im);
 
- protected:
+protected:
   void MakeTemplateWithCurrentParams();
   void FillTemplate(CVD::Image<float> &im, Params params);
   double Iterate(CVD::Image<CVD::byte> &im);
   Params mParams;
   CVD::Image<float> mimTemplate;
-  CVD::Image<Vector<2> > mimGradients;
-  CVD::Image<Vector<2> > mimAngleJacs;
-  
+  CVD::Image<Vector<2>> mimGradients;
+  CVD::Image<Vector<2>> mimAngleJacs;
+
   void MakeSharedTemplate();
   static CVD::Image<float> mimSharedSourceTemplate;
 
   double mdLastError;
 };
 
-
-
-
-#endif
+} // namespace PTAM
